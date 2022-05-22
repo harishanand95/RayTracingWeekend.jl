1c466c8d86ea25e6ffb538d40467e1a4b1dc8ffe
Harish <harishanand95@gmail.com>
2022-05-22 12:25:01 -0700

    add threads to 01, 09 examples

--- test/chapters/01_imagegradient_threads.jl
+++ test/chapters/01_imagegradient_threads.jl
@@ -2,16 +2,15 @@ using Images, ImageView
 using RayTracingWeekend
 using BenchmarkTools
 using Base.Threads
-using InteractiveUtils
 
 function render()
   # Generate an 256x256 image
-  height = 256
-  width = 256
+  height = trunc(256)
+  width = trunc(256)
   img = rand(RGB{Float32}, height, width)
 
   # julia is col major
-  for col in 1:width 
+  @threads for col in 1:width 
     for row in 1:height 
       img[row, col] = RGB{Float32}(col/256.0f0, (256.0f0-row)/256.0f0, 0.25f0)
     end
@@ -20,9 +19,5 @@ function render()
 end
 
 print(@__FILE__)
-time_f() = @time render()
-time_f() 
-time_f() 
-
-# @btime render()
+@btime render()
 save("imgs/01_imagegradient.png", render())
---
+++ test/chapters/09_skycolor_threads.jl
@@ -0,0 +1,49 @@
+using Images, ImageView
+using BenchmarkTools
+using RayTracingWeekend
+
+using Base.Threads
+
+function render()
+  # Image
+  aspect_ratio = Float32(16/9)
+  image_width  = Int32(400)
+  image_height = Int32(image_width / aspect_ratio)
+
+  # Camera geometry
+  # (0,0,0) is the origin of the Camera
+  # positive x-axis is (right-side) width 
+  # positive y-axis is upwards height
+  # negative z-axis points towards the imageplane (viewport)
+  # aspect ratio is 16:9
+  # viewport height = 2; width = 4
+  # bottom left (-2, -1, -1) 
+  viewport_height   = Float32(2.0)
+  viewport_width    = Float32(viewport_height * aspect_ratio)
+  focal_length      = Float32(1.0)
+  origin            = Point{Float32}(0, 0, 0)
+  horizontal        = Vec{Float32}(viewport_width, 0, 0) # h direction
+  vertical          = Vec{Float32}(0, viewport_height, 0) # veritcal direction
+  shift_in_z_axis   = Vec{Float32}(0.0, 0.0, -focal_length)
+  lower_left_corner = shift_in_z_axis - horizontal/2 - vertical/2
+
+  # Render
+  img = rand(RGB{Float32}, image_height, image_width)
+
+  # col-major
+  @threads for col in 1:image_width 
+    for row in 1:image_height 
+      u = Float32(col / image_width)
+      v = Float32((image_height-row) / image_height) # we start at the top left
+      # ray direction is lower_left_corner + component in x + component in y - origin(reference point)
+      # we need to create a vec to store the new direction, as its calculated as a difference in 2 points
+      ray = Ray(origin, lower_left_corner + u*horizontal + v*vertical)
+      img[row, col] = sky_color(ray)
+    end
+  end
+  img
+end
+
+print(@__FILE__)
+@btime render()
+save("imgs/09_skycolor.png", render())
